458d4bbd25ddee6df9b88d06fe61d0d7a774f2d5
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2020-08-17 13:34:50 +0300

    Переделана функция main
    Теперь запуск игры осуществляется с помощью класса Game

--- src/ru/ulteam8/evolution/Main.kt
+++ src/ru/ulteam8/evolution/Main.kt
@@ -1,24 +1,8 @@
 package ru.ulteam8.evolution
 
-import ru.ulteam8.evolution.graphics.Display
-import java.awt.event.ActionEvent
-import javax.swing.AbstractAction
-import javax.swing.Timer
-
+import ru.ulteam8.evolution.core.Game
 
 fun main() {
-    Display.create(800, 600, 3)
-
-    val timer = Timer(1000/60, MyAction())
-    timer.isRepeats = true
-    timer.start()
-}
-
-private class MyAction() : AbstractAction() {
-    override fun actionPerformed(e: ActionEvent?) {
-        Display.clear()
-        Display.render()
-        Display.swapBuffers()
-    }
-
+    val game = Game()
+    game.start()
 }
