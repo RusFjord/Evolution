d8e849997c797e9ab57b76e5c2793e1a96d0f90d
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2020-08-18 12:40:22 +0300

    Инициализация класса World

--- src/ru/ulteam8/evolution/core/World.kt
+++ src/ru/ulteam8/evolution/core/World.kt
@@ -1,5 +1,14 @@
 package ru.ulteam8.evolution.core
 
-class World {
+class World(val width: Int, val height: Int) {
+    val size = width * height
+    private val places = emptyArray<Place>()
 
+    init {
+        var i = 0
+        while(i < size) {
+            places[i] = Place.create()
+            i++
+        }
+    }
 }
