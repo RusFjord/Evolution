bde227bdba4f02cfd2e4dbf1b873e0d278b9a885
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2020-08-20 17:01:59 +0300

    Исправлен метод render класса World

--- src/ru/ulteam8/evolution/core/World.kt
+++ src/ru/ulteam8/evolution/core/World.kt
@@ -1,5 +1,11 @@
 package ru.ulteam8.evolution.core
 
+private const val NONE_FOOD_COLOR = 0xFFEAFAF1.toInt()
+private const val FEW_FOOD_COLOR = 0xFF82E0AA.toInt()
+private const val NORMAL_FOOD_COLOR = 0xFF28B463.toInt()
+private const val LOT_FOOD_COLOR = 0xFF186A3B.toInt()
+
+
 class World(val width: Int, val height: Int) : EvoObject {
     val size = width * height
     private val places = emptyArray<Place>()
@@ -7,7 +13,7 @@ class World(val width: Int, val height: Int) : EvoObject {
     init {
         var i = 0
         while(i < size) {
-            places[i] = Place.create()
+            places[i] = Place()
             i++
         }
     }
@@ -22,19 +28,19 @@ class World(val width: Int, val height: Int) : EvoObject {
         val graphics2D = Display.getGraphics()
         var i = 0
         while (i < size) {
+            var y = i % 600
+            var x = y + (i - y)
             if (places[i].entity != null) {
 
             } else {
-
-            }
-        }
-        places.forEach {
-            if (it.entity != null) {
-
-            } else {
-                when (it.foodLevel) {
-                    //in 0..99 ->
+                val colorInt = when (it.foodLevel) {
+                    0 -> NONE_FOOD_COLOR
+                    in 1..99 -> FEW_FOOD_COLOR
+                    in 100..199 -> NORMAL_FOOD_COLOR
+                    else LOT_FOOD_COLOR
                 }
+                graphics2D.setColor(Color(colorInt))
+                graphics2D.drawLine(x, y, x, y)
             }
         }
     }
