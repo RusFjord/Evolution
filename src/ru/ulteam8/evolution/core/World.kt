47a23a23ddd16b265d857740b0233a1f72a7750e
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2020-08-21 16:47:37 +0300

    Класс World
    Исправлена инициализация массива карты. Добавлен расчет координат по индексу массива

--- src/ru/ulteam8/evolution/core/World.kt
+++ src/ru/ulteam8/evolution/core/World.kt
@@ -1,5 +1,9 @@
 package ru.ulteam8.evolution.core
 
+import ru.ulteam8.evolution.graphics.Display
+import java.awt.Color
+import java.awt.Graphics2D
+
 private const val NONE_FOOD_COLOR = 0xFFEAFAF1.toInt()
 private const val FEW_FOOD_COLOR = 0xFF82E0AA.toInt()
 private const val NORMAL_FOOD_COLOR = 0xFF28B463.toInt()
@@ -8,15 +12,7 @@ private const val LOT_FOOD_COLOR = 0xFF186A3B.toInt()
 
 class World(val width: Int, val height: Int) : EvoObject {
     val size = width * height
-    private val places = emptyArray<Place>()
-
-    init {
-        var i = 0
-        while(i < size) {
-            places[i] = Place()
-            i++
-        }
-    }
+    private var places = Array<Place>(size, {it -> Place()})
 
     override fun update() {
         places.forEach {
@@ -25,23 +21,25 @@ class World(val width: Int, val height: Int) : EvoObject {
     }
 
     fun render() {
-        val graphics2D = Display.getGraphics()
+        val graphics2D = Display.getGraphics() as Graphics2D
         var i = 0
         while (i < size) {
-            var y = i % 600
-            var x = y + (i - y)
+            var y = i % width
+            var x = i - width * y
             if (places[i].entity != null) {
 
             } else {
-                val colorInt = when (it.foodLevel) {
+                val colorInt = when (places[i].foodLevel) {
                     0 -> NONE_FOOD_COLOR
                     in 1..99 -> FEW_FOOD_COLOR
                     in 100..199 -> NORMAL_FOOD_COLOR
-                    else LOT_FOOD_COLOR
+                    else -> LOT_FOOD_COLOR
                 }
-                graphics2D.setColor(Color(colorInt))
-                graphics2D.drawLine(x, y, x, y)
+
+                graphics2D!!.setColor(Color(colorInt))
+                graphics2D!!.drawLine(x, y, x, y)
             }
+            i++
         }
     }
 
